ef08b835e631311047cb53796ead4a532c9386fb
John Dowd <irishdowd10@gmail.com>
2017-08-04 12:06:36 -0700

    add create and edit page

--- GummyKingdom/Controllers/ProductsController.cs
+++ GummyKingdom/Controllers/ProductsController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using GummyKingdom.Models;
+using Microsoft.EntityFrameworkCore;
+
 
 
 namespace GummyKingdom.Controllers
@@ -21,5 +23,45 @@ namespace GummyKingdom.Controllers
 			var thisProduct = db.Products.FirstOrDefault(products => products.ProductId == id);
 			return View(thisProduct);
 		}
+		public IActionResult Create()
+		{
+			return View();
+		}
+		[HttpPost]
+		public IActionResult Create(Product product)
+		{
+			db.Products.Add(product);
+			db.SaveChanges();
+			return RedirectToAction("Index");
+		}
+		public IActionResult Edit(int id)
+		{
+			var thisProduct = db.Products.FirstOrDefault(products => products.ProductId == id);
+			return View(thisProduct);
+		}
+
+		[HttpPost]
+		public IActionResult Edit(Product product)
+		{
+			db.Entry(product).State = EntityState.Modified;
+			db.SaveChanges();
+			return RedirectToAction("Index");
+		}
+
+		public IActionResult Delete(int id)
+		{
+			var thisProduct = db.Products.FirstOrDefault(products => products.ProductId == id);
+			return View(thisProduct);
+		}
+
+		[HttpPost, ActionName("Delete")]
+		public IActionResult DeleteConfirmed(int id)
+		{
+			var thisProduct = db.Products.FirstOrDefault(products => products.ProductId == id);
+			db.Products.Remove(thisProduct);
+			db.SaveChanges();
+			return RedirectToAction("Index");
+		}
+
 	}
 }
